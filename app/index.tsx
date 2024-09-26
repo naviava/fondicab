e9b80d99153c18aa8b1da0ce7b3ffc651d6ff62c
Navin Avadhani <knavinavadhani@gmail.com>
2024-09-26 20:20:52 +0530

    chore: Added prettier

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
       <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    </SafeAreaView>
   );
 }
