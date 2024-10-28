949a65013d564cc793fee7507b7e650d7abfd031
Navin Avadhani <knavinavadhani@gmail.com>
2024-10-28 17:11:44 +0530

    feat: Added "/user" api

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -1,9 +1,33 @@
-interface Props {
-  name: string;
-  email: string;
-  clerkId: string;
-}
+import { neon } from "@neondatabase/serverless";
 
 export async function POST(req: Request) {
-  const { name, email, clerkId }: Props = await req.json();
+  try {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const { name, email, clerkId } = await req.json();
+
+    if (!name || !email || !clerkId) {
+      return Response.json(
+        { error: "Missing required fields!" },
+        { status: 400 },
+      );
+    }
+
+    const res = await sql`
+    INSERT INTO users (
+      name,
+      email,
+      clerkId
+    )
+    VALUES (
+      ${name},
+      ${email},
+      ${clerkId}
+    )
+  `;
+
+    return new Response(JSON.stringify({ data: res }), { status: 201 });
+  } catch (e) {
+    console.log(e);
+    return Response.json({ error: e }, { status: 500 });
+  }
 }
